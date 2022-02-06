39d55b7fd08c56f19770dddebbbee36d5c6ed9e2
Midnights <michaelcasas13@gmail.com>
2022-02-06 02:01:46 -0600

    feature: context-based breakpoint listener finished

---
+++ src/components/util/layout.component.tsx
@@ -0,0 +1,22 @@
+import React, {  useEffect, useLayoutEffect } from "react";
+import { useWindowDimensions } from "react-native";
+import { layoutContext } from "../../context";
+import { determineLayout } from "../../functions/util/layout.function";
+import { useLayout } from "../../hooks/layout.hooks";
+
+export default function ({ children }: { children: React.ReactNode }): JSX.Element {
+
+  const layout = useLayout();
+  const { width } = useWindowDimensions();
+
+  // Only Re-Render the children if a layout breakpoint has been hit
+  useEffect(() => {
+    layout.setLayout(determineLayout(width));
+  }, [width]);
+
+  return (
+    <layoutContext.Provider value={layout}>
+      { children }
+    </layoutContext.Provider>
+  );
+}
