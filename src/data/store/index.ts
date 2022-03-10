7a673d16e2ecb68dcf102961c9c28ebb1a2a107c
ItsMidnights <michaelcasas13@gmail.com>
2022-03-10 11:35:31 -0600

    refactor: layout slice init

--- src/data/store/index.ts
+++ src/data/store/index.ts
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { layoutSlice } from "./layout.slice";
 
-export default configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    layout: layoutSlice.reducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
---
+++ src/data/store/layout.slice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from ".";
+import { LayoutType } from "../../types";
+
+interface Layout {
+  value: LayoutType;
+}
+
+export const initialState: Layout = {
+  value: "mobile",
+};
+
+export const layoutSlice = createSlice({
+  name: "layout",
+  initialState: initialState,
+  reducers: {
+    update: (state, action: PayloadAction<LayoutType>) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const selectLayout = (state: RootState) => state.layout.value;
