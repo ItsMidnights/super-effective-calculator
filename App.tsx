00896ebade77c038118e96af1bf2936ac9702ccd
ItsMidnights <michaelcasas13@gmail.com>
2022-03-10 11:21:32 -0600

    refactor: redux toolkit init

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
+import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
-import { Routes } from "./src/routes";
+import Store from "./src/data/store";
 import LayoutProvider from "./src/context/layout/layout.provider";
-import { SafeAreaView, Platform, StatusBar } from "react-native";
-import { primary_color } from "./assets/colors";
+import { Routes } from "./src/routes";
+import { Platform, StatusBar } from "react-native";
 import { injectWebCss } from "./src/functions/util";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <LayoutProvider>
-        {/* <SafeAreaView style={{ flex: 1, backgroundColor: primary_color }}> */}
-        <StatusBar
-          barStyle={"light-content"}
-          backgroundColor={"#998FC7"}
-          // backgroundColor={primary_color}
-        />
-        <Routes />
-        {/* </SafeAreaView> */}
-      </LayoutProvider>
+      <Provider store={Store}>
+        <LayoutProvider>
+          <StatusBar barStyle={"light-content"} backgroundColor={"#998FC7"} />
+          <Routes />
+        </LayoutProvider>
+      </Provider>
     </NavigationContainer>
   );
 }
