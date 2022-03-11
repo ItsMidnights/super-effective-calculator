4e24168ac90cd5cde4d4ea8584798c050edf0b8e
ItsMidnights <michaelcasas13@gmail.com>
2022-03-11 00:00:14 -0600

    refactor: redux ripped out

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
-import Store from "./src/data/store";
 import LayoutProvider from "./src/context/layout/layout.provider";
 import { Routes } from "./src/routes";
 import { Platform, StatusBar } from "react-native";
@@ -9,12 +8,10 @@ import { injectWebCss } from "./src/functions/util";
 export default function App() {
   return (
     <NavigationContainer>
-      <Provider store={Store}>
-        <LayoutProvider>
-          <StatusBar barStyle={"light-content"} backgroundColor={"#998FC7"} />
-          <Routes />
-        </LayoutProvider>
-      </Provider>
+      <LayoutProvider>
+        <StatusBar barStyle={"light-content"} backgroundColor={"#998FC7"} />
+        <Routes />
+      </LayoutProvider>
     </NavigationContainer>
   );
 }
--- src/context/layout/layout.provider.tsx
+++ src/context/layout/layout.provider.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { Dimensions } from "react-native";
-import { withDecay } from "react-native-reanimated";
-import { useDispatch, useSelector } from "react-redux";
-import { MOBILE_MAX_WIDTH } from "../../constants";
-import { selectLayout, update } from "../../data/store/layout.slice";
 import { determineLayout } from "../../functions/util/layout.function";
 import { SafeAreaWrapper } from "../../views/util/SafeAreaWrapper";
 
@@ -12,22 +8,16 @@ export default function ({
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  const layout = useSelector(selectLayout);
-  const dispatch = useDispatch();
-
   // Only Re-Render the children if a layout breakpoint has been hit
   React.useEffect(() => {
     const _unsubscribe = Dimensions.addEventListener("change", ({ window }) => {
       const { width } = window;
       const currentLayout = determineLayout(width);
-      if (currentLayout != layout) {
-        dispatch(update(currentLayout));
-      }
+      console.log(currentLayout);
     });
-    console.log("Current Layout:\n", layout);
 
     return _unsubscribe;
-  }, [layout]);
+  }, []);
 
   return <SafeAreaWrapper>{children}</SafeAreaWrapper>;
 }
